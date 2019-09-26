fa375e93d4f053afc320fed235d450fae69c6b6b
fapcod <frankantoniopiato@gmail.com>
2019-09-26 09:51:54 -0500

    calculadora hecha en visual basic

---
+++ CalculadoraV2/Form1.Designer.vb
@@ -0,0 +1,376 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtPantalla = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnBorrarTodo = New System.Windows.Forms.Button()
+        Me.btnBorrarDerecha = New System.Windows.Forms.Button()
+        Me.btnDivision = New System.Windows.Forms.Button()
+        Me.btnMultiplicacion = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btnResta = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btnSuma = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btnIgual = New System.Windows.Forms.Button()
+        Me.btnPunto = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnMasMenos = New System.Windows.Forms.Button()
+        Me.FapCod = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtPantalla
+        '
+        Me.txtPantalla.BackColor = System.Drawing.Color.Turquoise
+        Me.txtPantalla.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.txtPantalla.Cursor = System.Windows.Forms.Cursors.No
+        Me.txtPantalla.Font = New System.Drawing.Font("Consolas", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPantalla.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.txtPantalla.Location = New System.Drawing.Point(21, 27)
+        Me.txtPantalla.Name = "txtPantalla"
+        Me.txtPantalla.ReadOnly = True
+        Me.txtPantalla.Size = New System.Drawing.Size(371, 51)
+        Me.txtPantalla.TabIndex = 0
+        Me.txtPantalla.Text = "0"
+        Me.txtPantalla.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Button1
+        '
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.Button1.Location = New System.Drawing.Point(21, 109)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(58, 49)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "C"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnBorrarTodo
+        '
+        Me.btnBorrarTodo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnBorrarTodo.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnBorrarTodo.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btnBorrarTodo.Location = New System.Drawing.Point(116, 109)
+        Me.btnBorrarTodo.Name = "btnBorrarTodo"
+        Me.btnBorrarTodo.Size = New System.Drawing.Size(58, 49)
+        Me.btnBorrarTodo.TabIndex = 2
+        Me.btnBorrarTodo.Text = "CE"
+        Me.btnBorrarTodo.UseVisualStyleBackColor = True
+        '
+        'btnBorrarDerecha
+        '
+        Me.btnBorrarDerecha.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnBorrarDerecha.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnBorrarDerecha.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btnBorrarDerecha.Location = New System.Drawing.Point(216, 109)
+        Me.btnBorrarDerecha.Name = "btnBorrarDerecha"
+        Me.btnBorrarDerecha.Size = New System.Drawing.Size(58, 49)
+        Me.btnBorrarDerecha.TabIndex = 3
+        Me.btnBorrarDerecha.Text = "<<"
+        Me.btnBorrarDerecha.UseVisualStyleBackColor = True
+        '
+        'btnDivision
+        '
+        Me.btnDivision.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnDivision.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnDivision.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btnDivision.Location = New System.Drawing.Point(334, 109)
+        Me.btnDivision.Name = "btnDivision"
+        Me.btnDivision.Size = New System.Drawing.Size(58, 49)
+        Me.btnDivision.TabIndex = 4
+        Me.btnDivision.Text = "/"
+        Me.btnDivision.UseVisualStyleBackColor = True
+        '
+        'btnMultiplicacion
+        '
+        Me.btnMultiplicacion.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnMultiplicacion.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnMultiplicacion.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btnMultiplicacion.Location = New System.Drawing.Point(334, 164)
+        Me.btnMultiplicacion.Name = "btnMultiplicacion"
+        Me.btnMultiplicacion.Size = New System.Drawing.Size(58, 49)
+        Me.btnMultiplicacion.TabIndex = 8
+        Me.btnMultiplicacion.Text = "x"
+        Me.btnMultiplicacion.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn9.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn9.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btn9.Location = New System.Drawing.Point(216, 164)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(58, 49)
+        Me.btn9.TabIndex = 7
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn8.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn8.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btn8.Location = New System.Drawing.Point(116, 164)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(58, 49)
+        Me.btn8.TabIndex = 6
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn7.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn7.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btn7.Location = New System.Drawing.Point(21, 164)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(58, 49)
+        Me.btn7.TabIndex = 5
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btnResta
+        '
+        Me.btnResta.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnResta.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnResta.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btnResta.Location = New System.Drawing.Point(334, 219)
+        Me.btnResta.Name = "btnResta"
+        Me.btnResta.Size = New System.Drawing.Size(58, 49)
+        Me.btnResta.TabIndex = 12
+        Me.btnResta.Text = "-"
+        Me.btnResta.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn6.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn6.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btn6.Location = New System.Drawing.Point(216, 219)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(58, 49)
+        Me.btn6.TabIndex = 11
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn5.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btn5.Location = New System.Drawing.Point(116, 219)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(58, 49)
+        Me.btn5.TabIndex = 10
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn4.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btn4.Location = New System.Drawing.Point(21, 219)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(58, 49)
+        Me.btn4.TabIndex = 9
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btnSuma
+        '
+        Me.btnSuma.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnSuma.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnSuma.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btnSuma.Location = New System.Drawing.Point(334, 274)
+        Me.btnSuma.Name = "btnSuma"
+        Me.btnSuma.Size = New System.Drawing.Size(58, 49)
+        Me.btnSuma.TabIndex = 16
+        Me.btnSuma.Text = "+"
+        Me.btnSuma.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn3.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btn3.Location = New System.Drawing.Point(216, 274)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(58, 49)
+        Me.btn3.TabIndex = 15
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn2.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btn2.Location = New System.Drawing.Point(116, 274)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(58, 49)
+        Me.btn2.TabIndex = 14
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn1.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btn1.Location = New System.Drawing.Point(21, 274)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(58, 49)
+        Me.btn1.TabIndex = 13
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btnIgual
+        '
+        Me.btnIgual.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnIgual.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnIgual.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btnIgual.Location = New System.Drawing.Point(334, 329)
+        Me.btnIgual.Name = "btnIgual"
+        Me.btnIgual.Size = New System.Drawing.Size(58, 49)
+        Me.btnIgual.TabIndex = 20
+        Me.btnIgual.Text = "="
+        Me.btnIgual.UseVisualStyleBackColor = True
+        '
+        'btnPunto
+        '
+        Me.btnPunto.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnPunto.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnPunto.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btnPunto.Location = New System.Drawing.Point(216, 329)
+        Me.btnPunto.Name = "btnPunto"
+        Me.btnPunto.Size = New System.Drawing.Size(58, 49)
+        Me.btnPunto.TabIndex = 19
+        Me.btnPunto.Text = "."
+        Me.btnPunto.UseVisualStyleBackColor = True
+        '
+        'btn0
+        '
+        Me.btn0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn0.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn0.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btn0.Location = New System.Drawing.Point(116, 329)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(58, 49)
+        Me.btn0.TabIndex = 18
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'btnMasMenos
+        '
+        Me.btnMasMenos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btnMasMenos.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnMasMenos.Font = New System.Drawing.Font("Code Bold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.btnMasMenos.Location = New System.Drawing.Point(21, 329)
+        Me.btnMasMenos.Name = "btnMasMenos"
+        Me.btnMasMenos.Size = New System.Drawing.Size(58, 49)
+        Me.btnMasMenos.TabIndex = 17
+        Me.btnMasMenos.Text = "+/-"
+        Me.btnMasMenos.UseVisualStyleBackColor = True
+        '
+        'FapCod
+        '
+        Me.FapCod.AutoSize = True
+        Me.FapCod.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.FapCod.Font = New System.Drawing.Font("MOAM91", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FapCod.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.FapCod.Location = New System.Drawing.Point(142, 399)
+        Me.FapCod.Name = "FapCod"
+        Me.FapCod.Size = New System.Drawing.Size(122, 23)
+        Me.FapCod.TabIndex = 21
+        Me.FapCod.Text = "By FapCod"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Highlight
+        Me.ClientSize = New System.Drawing.Size(405, 441)
+        Me.Controls.Add(Me.FapCod)
+        Me.Controls.Add(Me.btnIgual)
+        Me.Controls.Add(Me.btnPunto)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btnMasMenos)
+        Me.Controls.Add(Me.btnSuma)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btnResta)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btnMultiplicacion)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btnDivision)
+        Me.Controls.Add(Me.btnBorrarDerecha)
+        Me.Controls.Add(Me.btnBorrarTodo)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtPantalla)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents btnBorrarTodo As System.Windows.Forms.Button
+    Friend WithEvents btnBorrarDerecha As System.Windows.Forms.Button
+    Friend WithEvents btnDivision As System.Windows.Forms.Button
+    Friend WithEvents btnMultiplicacion As System.Windows.Forms.Button
+    Friend WithEvents btn9 As System.Windows.Forms.Button
+    Friend WithEvents btn8 As System.Windows.Forms.Button
+    Friend WithEvents btn7 As System.Windows.Forms.Button
+    Friend WithEvents btnResta As System.Windows.Forms.Button
+    Friend WithEvents btn6 As System.Windows.Forms.Button
+    Friend WithEvents btn5 As System.Windows.Forms.Button
+    Friend WithEvents btn4 As System.Windows.Forms.Button
+    Friend WithEvents btnSuma As System.Windows.Forms.Button
+    Friend WithEvents btn3 As System.Windows.Forms.Button
+    Friend WithEvents btn2 As System.Windows.Forms.Button
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents btnIgual As System.Windows.Forms.Button
+    Friend WithEvents btnPunto As System.Windows.Forms.Button
+    Friend WithEvents btn0 As System.Windows.Forms.Button
+    Friend WithEvents btnMasMenos As System.Windows.Forms.Button
+    Friend WithEvents FapCod As System.Windows.Forms.Label
+    Friend WithEvents txtPantalla As System.Windows.Forms.TextBox
+
+End Class
